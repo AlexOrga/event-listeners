213413574b456f1461de68541449c96643c5e092
Alex Orga <alexorga.dev@gmail.com>
2018-03-24 11:46:32 -0500

    changed names to green when button is clicked

--- main.js
+++ main.js
@@ -71,14 +71,28 @@ const buildDomString = (studentArray) => {
         domString += `</div>`;
     });
     printToDom(domString, "card-holder");    
+};
+
+
+
+const addAllEventListeners = () => {
+    const allTheButtons = document.getElementsByClassName('card-button');
+
+    for (let i = 0; i < allTheButtons.length; i ++) {
+        allTheButtons[i].addEventListener('click', changeNameToGreen);
+    }
+};
+
+const changeNameToGreen = (e) => {
+    const nameOfStudent = e.target.parentNode.children[0];
+    nameOfStudent.classList.add('green');
 }
 
-buildDomString(students);
 
-const allTheButtons = document.getElementsByClassName('card-button');
 
-for (let i = 0; i < allTheButtons.length; i ++) {
-    allTheButtons[i].addEventListener('click', (e) => {
-        console.log('event', e);
-    })
+const startApplication = () => {
+    buildDomString(students);
+    addAllEventListeners();
 }
+
+startApplication();
